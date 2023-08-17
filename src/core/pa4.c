0b41c7d3c9a549c592fa7692aec4316bd989fc33
neoterux <labfernandez2014@gmail.com>
2023-08-16 23:58:28 -0500

    Added functions for help

---
+++ src/core/pa4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pa4.h"
+
+void version(char *exec) {
+    printf(
+        "%s - %s\n"
+        "Written by Neoterux\n", exec, PA4_VERSION);
+}
+
+void usage(char *exec) {
+    printf("Usage: %s [options] <input_file> <output_file>\n", exec);
+    printf(
+        "OPTIONS:\n"
+        "-v|--version\tPrint the version information.\n"
+        "-h|--help\tPrint this message.\n"
+        "-w|--workers <n>\tSetup the worker thread count for image processing. "
+            "If set <= 0 then the value will be the same as CPU count. [default=4]\n"
+        "-p|--preview\tShow the preview of the images on terminals that can support it. [default=false]\n"
+        "-V|--vertical\tApply the filter on vertical. [default=false]\n"
+        "-i|--inverse\tApply the filter inverting the order of filters [default=false].\n"
+    );
+}
--- src/core/pa4.h
+++ src/core/pa4.h
@@ -1,4 +1,28 @@
+/**
+ * @file pa4.h
+ * @author Neoterux (labfernandez2014@gmail.com)
+ * @brief This file contains functions used for the executable itself rather than the solution.
+ * @version 0.1
+ * @date 2023-08-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 #ifndef PA_4_H
 #define PA_4_H
 #define PA4_VERSION "v0.01"
+
+typedef unsigned char bool;
+
+struct progopt {
+    int threads;
+    bool inverse;
+    bool horizontal;
+    bool preview;
+};
+
+void usage(char *exec);
+
+void version(char *exec);
+
 #endif
