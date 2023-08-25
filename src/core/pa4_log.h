b4a53270058bad0bc7d26a3b0ba7815536b9df99
Neoterux <labfernandez2014@gmail.com>
2023-08-25 08:25:30 -0500

    deleted not used files

--- src/core/pa4_log.h
+++
@@ -1,8 +0,0 @@
-////////////////////////////////////
-//      Internal login file     ////
-//  This is only for debugging  ////
-////////////////////////////////////
-#ifndef PA4_LOG
-#define PA4_LOG
-
-#endif
